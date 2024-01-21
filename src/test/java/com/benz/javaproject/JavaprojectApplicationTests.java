6b07012890390655781c523e4f6d5a5bde3c4f91
elanuralp <62593720+elanuralp@users.noreply.github.com>
2024-01-21 16:10:44 +0300

    initial commit

---
+++ src/test/java/com/benz/javaproject/JavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benz.javaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
